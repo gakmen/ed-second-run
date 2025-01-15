6a5e8cc4c62b114b8c0b93dd2592fa164367cfb1
Gosha Akmen <gakmen@icloud.com>
2025-01-16 01:06:58 +0500

    Create project

---
+++ EssentialFeed/EssentialFeedTests/EssentialFeedTests.swift
@@ -0,0 +1,17 @@
+//
+//  EssentialFeedTests.swift
+//  EssentialFeedTests
+//
+//  Created by Gosha Akmen on 16.01.2025.
+//
+
+import Testing
+@testable import EssentialFeed
+
+struct EssentialFeedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
