7b9893ad1f3615562032c0991dde600e16786172
Gosha Akmen <gakmen@icloud.com>
2025-01-18 13:14:15 +0500

    Move `RemoteFeedLoader` components to production

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -1,23 +1,6 @@
-import Testing
+import EssentialFeed
 import Foundation
-
-class RemoteFeedLoader {
-  let client: HTTPClient
-  let url: URL
-
-  init(url: URL, client: HTTPClient) {
-    self.url = url
-    self.client = client
-  }
-
-  func load() {
-    client.get(from: url)
-  }
-}
-
-protocol HTTPClient {
-  func get(from url: URL)
-}
+import Testing
 
 struct RemoteFeedLoaderTests {
   @Test func init_doesNotRequestDataFromURL() {
@@ -52,5 +35,5 @@ struct RemoteFeedLoaderTests {
       requestedURL = url
     }
   }
-  
+
 }
