9b0dfdf89caa48d7b1c14476fdab0014912ec117
Gosha Akmen <gakmen@icloud.com>
2025-01-20 00:56:41 +0500

    Create intermediate type `Item` for mapping purposes to avoid using Coding Keys and thus coupling Feed Feature with API implementation

--- EssentialFeed/EssentialFeed/Feed Feature/FeedItem.swift
+++ EssentialFeed/EssentialFeed/Feed Feature/FeedItem.swift
@@ -1,8 +1,20 @@
 import Foundation
 
-public struct FeedItem: Decodable, Equatable {
-  let id: UUID
-  let description: String?
-  let location: String?
-  let imageURL: URL
+public struct FeedItem: Equatable {
+  public let id: UUID
+  public let description: String?
+  public let location: String?
+  public let imageURL: URL
+
+  public init(
+    id: UUID,
+    description: String? = nil,
+    location: String? = nil,
+    imageURL: URL
+  ) {
+    self.id = id
+    self.description = description
+    self.location = location
+    self.imageURL = imageURL
+  }
 }
