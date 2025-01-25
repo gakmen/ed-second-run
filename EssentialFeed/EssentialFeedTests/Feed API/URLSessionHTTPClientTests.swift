359ec774f2be364cb71c208f233a86987869e5b3
Gosha Akmen <gakmen@icloud.com>
2025-01-26 00:13:38 +0500

    Fix indentation

--- EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -46,56 +46,56 @@ final class URLSessionHTTPClientTests {
 
   // MARK: - Helpers
 
-    private class URLProtocolStub: URLProtocol {
-      private static var stubs = [URL: Stub]()
+  private class URLProtocolStub: URLProtocol {
+    private static var stubs = [URL: Stub]()
 
-      private struct Stub {
-        let data: Data?
-        let response: URLResponse?
-        let error: Error?
-      }
-
-      static func stub(url: URL, data: Data?, response: URLResponse?, error: Error?) {
-        stubs[url] = Stub(data: data, response: response, error: error)
-      }
-
-      static func startInterceptingRequests() {
-        URLProtocol.registerClass(URLProtocolStub.self)
-      }
-
-      static func stopInterceptingRequests() {
-        URLProtocol.unregisterClass(URLProtocolStub.self)
-        stubs = [:]
-      }
-
-      override class func canInit(with request: URLRequest) -> Bool {
-        guard let url = request.url else { return false }
-
-        return URLProtocolStub.stubs[url] != nil
-      }
-
-      override class func canonicalRequest(for request: URLRequest) -> URLRequest {
-        return request
-      }
-
-      override func startLoading() {
-        guard let url = request.url, let stub = URLProtocolStub.stubs[url] else { return }
-
-        if let data = stub.data {
-          client?.urlProtocol(self, didLoad: data)
-        }
-
-        if let response = stub.response {
-          client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
-        }
-
-        if let error = stub.error {
-          client?.urlProtocol(self, didFailWithError: error)
-        }
-
-        client?.urlProtocolDidFinishLoading(self)
-      }
-
-      override func stopLoading() {}
+    private struct Stub {
+      let data: Data?
+      let response: URLResponse?
+      let error: Error?
     }
+
+    static func stub(url: URL, data: Data?, response: URLResponse?, error: Error?) {
+      stubs[url] = Stub(data: data, response: response, error: error)
+    }
+
+    static func startInterceptingRequests() {
+      URLProtocol.registerClass(URLProtocolStub.self)
+    }
+
+    static func stopInterceptingRequests() {
+      URLProtocol.unregisterClass(URLProtocolStub.self)
+      stubs = [:]
+    }
+
+    override class func canInit(with request: URLRequest) -> Bool {
+      guard let url = request.url else { return false }
+
+      return URLProtocolStub.stubs[url] != nil
+    }
+
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+      return request
+    }
+
+    override func startLoading() {
+      guard let url = request.url, let stub = URLProtocolStub.stubs[url] else { return }
+
+      if let data = stub.data {
+        client?.urlProtocol(self, didLoad: data)
+      }
+
+      if let response = stub.response {
+        client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+      }
+
+      if let error = stub.error {
+        client?.urlProtocol(self, didFailWithError: error)
+      }
+
+      client?.urlProtocolDidFinishLoading(self)
+    }
+
+    override func stopLoading() {}
+  }
 }
