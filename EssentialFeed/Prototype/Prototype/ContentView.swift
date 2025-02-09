45ce277b459c0d3ec60fdbd909023530797a0d8d
Gosha Akmen <gakmen@icloud.com>
2025-02-09 13:47:30 +0500

    Add empty project for prototype with "Hello, World" template

---
+++ EssentialFeed/Prototype/Prototype/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Prototype
+//
+//  Created by Gosha Akmen on 09.02.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EssentialFeed/Prototype/Prototype/PrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrototypeApp.swift
+//  Prototype
+//
+//  Created by Gosha Akmen on 09.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct PrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
