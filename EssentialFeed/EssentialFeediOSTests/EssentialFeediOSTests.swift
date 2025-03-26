ea49f40fe0e1dfc32b264a775ac1bcb2a665c142
Gosha Akmen <gakmen@icloud.com>
2025-03-26 07:53:04 +0500

    Introduce `EssentialFeediOSTests`

--- EssentialFeed/EssentialFeediOSTests/EssentialFeediOSTests.swift
+++ EssentialFeed/EssentialFeediOSTests/EssentialFeediOSTests.swift
@@ -1,8 +1,53 @@
-import Testing
+import SwiftUI
+import ViewInspector
+import XCTest
 
-struct EssentialFeediOSTests {
+struct FeedView: View {
+  @State var loader: LoaderSpy
+  var didAppear: ((Self) -> Void)?
 
-  @Test func example() async throws {
+  init(loader: LoaderSpy) {
+    self.loader = loader
+  }
 
+  var body: some View {
+    Text("FeedView")
+      .onAppear {
+        loader.load()
+        self.didAppear?(self)
+      }
+  }
+}
+
+class EssentialFeediOSXCTests: XCTestCase {
+  func test_init_doesNotLoadFeed() {
+    let loader = LoaderSpy()
+    var _ = FeedView(loader: loader)
+    XCTAssertEqual(loader.loadCallCount, 0)
+  }
+  
+  func test_onAppear_loadsFeed() throws {
+    let loader = LoaderSpy()
+    var sut = FeedView(loader: loader)
+
+    let exp = expectation(description: "Wait for didAppear to be called")
+    sut.didAppear = { view in
+      XCTAssertEqual(view.loader.loadCallCount, 1)
+      exp.fulfill()
+    }
+
+    ViewHosting.host(view: sut)
+    defer { ViewHosting.expel() }
+    wait(for: [exp], timeout: 0.01)
+  }
+}
+
+//MARK: - Helpers
+
+class LoaderSpy {
+  private(set) var loadCallCount = 0
+
+  func load() {
+    loadCallCount += 1
   }
 }
