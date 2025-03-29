3c18369b1e096c3fa0eafc687b99fbd37fd5f6b6
Gosha Akmen <gakmen@icloud.com>
2025-03-29 17:38:22 +0500

    Substitute `LoaderSpy` with the production `FeedLoader` protocol abstraction

--- EssentialFeed/EssentialFeediOSTests/EssentialFeediOSTests.swift
+++ EssentialFeed/EssentialFeediOSTests/EssentialFeediOSTests.swift
@@ -1,20 +1,25 @@
+import EssentialFeed
 import SwiftUI
 import ViewInspector
 import XCTest
 
 struct FeedView: View {
-  @State var loader: LoaderSpy
+  @State var loader: FeedLoader
   var didAppear: ((Self) -> Void)?
 
-  init(loader: LoaderSpy) {
+  init(loader: FeedLoader) {
     self.loader = loader
   }
 
   var body: some View {
     Text("FeedView")
       .onAppear {
-        loader.load()
-        self.didAppear?(self)
+        Task {
+          do {
+            _ = try await loader.load()
+            self.didAppear?(self)
+          } catch {}
+        }
       }
   }
 }
@@ -25,29 +30,32 @@ class EssentialFeediOSXCTests: XCTestCase {
     var _ = FeedView(loader: loader)
     XCTAssertEqual(loader.loadCallCount, 0)
   }
-  
-  func test_onAppear_loadsFeed() throws {
+
+  @MainActor
+  func test_onAppear_loadsFeed() async throws {
     let loader = LoaderSpy()
     var sut = FeedView(loader: loader)
 
     let exp = expectation(description: "Wait for didAppear to be called")
     sut.didAppear = { view in
-      XCTAssertEqual(view.loader.loadCallCount, 1)
+      XCTAssertEqual(loader.loadCallCount, 1)
       exp.fulfill()
     }
 
     ViewHosting.host(view: sut)
-    defer { ViewHosting.expel() }
-    wait(for: [exp], timeout: 0.01)
+    await fulfillment(of: [exp], timeout: 0.01)
+    ViewHosting.expel()
   }
 }
 
 //MARK: - Helpers
 
-class LoaderSpy {
+class LoaderSpy: FeedLoader {
+
   private(set) var loadCallCount = 0
 
-  func load() {
+  func load() async throws -> [EssentialFeed.FeedItem] {
     loadCallCount += 1
+    return [FeedItem(id: UUID(), imageURL: URL(string: "http://some-url.ru")!)]
   }
 }
