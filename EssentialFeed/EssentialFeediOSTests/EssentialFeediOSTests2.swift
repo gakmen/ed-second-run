57dab35cfacd62d451cb6d009864628634103b0a
Gosha Akmen <gakmen@icloud.com>
2025-04-13 14:30:12 +0500

    Attempt to use Swift Testing for SwiftUI

---
+++ EssentialFeed/EssentialFeediOSTests/EssentialFeediOSTests2.swift
@@ -0,0 +1,52 @@
+import EssentialFeed
+import SwiftUI
+import Testing
+import ViewInspector
+
+@Test func init_doesNotLoadFeed() {
+  let (_, loader) = makeSUT()
+
+  #expect(loader.loadCallCount == 0)
+}
+
+@MainActor
+@Test func onAppear_loadsFeed() async throws {
+  var (sut, loader) = makeSUT()
+
+  sut.onDidAppear = { _ in
+    #expect(loader.loadCallCount == 1)
+  }
+
+  ViewHosting.host(view: sut)
+  try await few(nanoseconds: 1_000_000)
+}
+
+// TODO: понять почему не работает тест при том, что брейкпоинт на confirm() срабатывает
+//@MainActor
+//@Test func onAppear_loadsFeed2() async throws {
+//  var (sut, loader) = makeSUT()
+//
+//  await confirmation { confirm in
+//    sut.onDidAppear = { _ in
+//      if loader.loadCallCount == 1 {
+//        confirm()
+//      }
+//    }
+//    ViewHosting.host(view: sut)
+//  }
+//}
+
+//MARK: - Helpers
+
+private func makeSUT() -> (sut: FeedView, loader: LoaderSpy) {
+  let loaderSpy = LoaderSpy()
+  let sut = FeedView(loader: loaderSpy)
+  return (sut, loaderSpy)
+}
+
+private func few(nanoseconds: UInt64) async throws {
+  await confirmation { fulfillment in
+    try? await Task.sleep(nanoseconds: nanoseconds)
+    fulfillment()
+  }
+}
